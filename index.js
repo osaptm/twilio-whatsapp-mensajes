1a7548c06a3044183f8986cece79988ad850e8c6
Mario Joel Olano Ramirez <exmilder@gmail.com>
2023-01-29 14:03:56 -0500

    OKI

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
+
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+
+app.use( cors() );
+app.use( express.json() );
 
 app.post('twilio',(req, res)=>{
     const twilio = new client.twiml.MessagingResponse();
