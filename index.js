21fe873c117038b45bf8a5215020f0f547cff71a
Mario Joel Olano Ramirez <exmilder@gmail.com>
2023-01-29 14:48:55 -0500

    final

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+//matar taskkill /F /IM node.exe
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilio = require('twilio')(accountSid, authToken);
-const response = new twilio.twiml.MessagingResponse();
+
 
 app.use( cors() );
 app.use( express.json() );
 
 app.post('/twilio',(req, res)=>{
-    console.log( "Holaaaaaaaaa", response.toString() )
-    response.message('Gracias por enviarnos un mensaje');
+    console.log( "Holaaaaaaaaa", req.body )
     res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(response.toString());
+    res.end();
 });
 
 app.post('/status-twilio',(req, res)=>{
