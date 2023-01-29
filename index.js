31a259c6c4c8cfe87e19289b478f1834322efaee
Mario Joel Olano Ramirez <exmilder@gmail.com>
2023-01-29 13:04:17 -0500

    OK2

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 require('dotenv')();
 const app = express();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 app.post('twilio',(req, res)=>{
     const twilio = new twilio.twiml.MessagingResponse();
     console.log( twilio )
