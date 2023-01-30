b1c1921746526af098a6eddf64eb5d2779814e2e
Mario Joel Olano Ramirez <exmilder@gmail.com>
2023-01-30 11:39:35 -0500

    final

--- index.js
+++ index.js
@@ -9,15 +9,32 @@ const app = express();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilio = require('twilio')(accountSid, authToken);
-
+const MessagingResponse = require('twilio').twiml.MessagingResponse; 
 
 app.use( cors() );
 app.use( express.json() );
 
 app.post('/twilio',(req, res)=>{
-    console.log( "Holaaaaaaaaa", req.body )
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end();
+
+    const twiml = new MessagingResponse();
+
+    if (req.body.NumMedia > 0) {
+      // Message with media
+      const mediaUrl = twilio.uri(req.body.MediaUrl0);
+      const mediaType = req.body.MediaContentType0;  
+      twiml.message(
+        `Media received. Type: ${mediaType}. Download it from ${mediaUrl}`
+      );
+    } else {
+      // Text message
+      const text = req.body.Body;
+      console.log(text,">>>>>>>>>>>>>>>>>")
+      twiml.message(`You said: ${text}`);
+    }
+  
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+
 });
 
 app.post('/status-twilio',(req, res)=>{
